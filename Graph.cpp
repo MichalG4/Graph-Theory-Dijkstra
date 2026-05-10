781cde9d64213c23bec85c269da4362b8da16bb1
MichalG4 <michal.gebka0@gmail.com>
2026-05-10 21:11:02 +0200

    added inheritance and AdjencencyList::AddEdge(), and ListNode

--- Graph.cpp
+++ Graph.cpp
@@ -8,28 +8,66 @@
 // error handling at connecting one point to self
 AdjacencyMatrix::AdjacencyMatrix()
 {
-    Matrix=std::vector<std::vector<int>>(V, std::vector<int>(V,0));
+    Matrix=std::vector<std::vector<int>>(V, std::vector<int>(V,-1));
+    size=V;
+    edges=0;
 }
 AdjacencyMatrix::AdjacencyMatrix(int SIZE)
 {
-    Matrix=std::vector<std::vector<int>>(SIZE, std::vector<int>(SIZE,0));
+    Matrix=std::vector<std::vector<int>>(SIZE, std::vector<int>(SIZE,-1));
+    size=SIZE;
+    edges=0;
 }
 AdjacencyMatrix::~AdjacencyMatrix(){}
 void AdjacencyMatrix::AddEdge(int x,int y,int weight)
 {
-    if(x!=y)
+    if(x!=y && x<size && y<size && Matrix[x][y]==-1)
     {
     Matrix[x][y]=weight;
     Matrix[y][x]=weight;   
+    edges++;
     }
+}
+
+void AdjacencyMatrix::RemoveEdge(int x,int y)
+{
+    if(x<size && y<size)
+    {
+        Matrix[x][y]=-1;
+        Matrix[y][x]=-1;
+    }
+}
+void AdjacencyMatrix::CountDensity()
+{
+    density=edges/(size*(size-1));
+}
+float AdjacencyMatrix::GiveDensity()
+{
+    return density;
+}
+ListNode::ListNode(int WEIGHT,int VERTICE)
+{
+    weight=WEIGHT;
+    vertice=VERTICE;
 }
 AdjacencyList::AdjacencyList()
 {
-    List=std::vector<std::vector<int>>(V, std::vector<int>(0,0));
+    List=std::vector<std::vector<ListNode>>(V, std::vector<ListNode>(0));
+    size=V;
+    edges=0;
 }
 AdjacencyList::AdjacencyList(int SIZE)
 {
-    List=std::vector<std::vector<int>>(SIZE, std::vector<int>(0,0));
+    List=std::vector<std::vector<ListNode>>(SIZE, std::vector<ListNode>(0));
+    size=SIZE;
+    edges=0;
 }
 AdjacencyList::~AdjacencyList(){}
-
+void AdjacencyList::AddEdge(int x,int y,int weight)
+{
+    if(x!=y && x<size && y<size) //checking if it is not present (-1) would be ineficient 
+    {
+        List[x].push_back(ListNode(weight,y));
+        List[y].push_back(ListNode(weight,x));
+    }
+}
--- Graph.h
+++ Graph.h
@@ -3,22 +3,48 @@
 
 struct IGraph
 {
+    public:
+    virtual void AddEdge(int x,int y,int weight)=0;
+    virtual void RemoveEdge(int x,int y)=0;
+    virtual void CountDensity()=0;
+    virtual float GiveDensity()=0;
+    virtual ~IGraph() = default;
+    protected:
+    float density;
+    int size; //number of vertices
+    int edges;
 
 };
-struct AdjacencyMatrix
+struct AdjacencyMatrix:public IGraph
 {
     public:
     AdjacencyMatrix();
     AdjacencyMatrix(int SIZE);
     ~AdjacencyMatrix();
     void AddEdge(int x,int y,int weight);
+    void RemoveEdge(int x,int y);
+    void CountDensity();
+    float GiveDensity();
     private:
     std::vector<std::vector<int>> Matrix; 
 };
-struct AdjacencyList
+struct ListNode
 {
-    std::vector<std::vector<int>> List; 
+    public:
+    int vertice;
+    int weight;
+    ListNode(int weight,int vertice);
+};
+struct AdjacencyList:public IGraph
+{
+    public:
     AdjacencyList();
     AdjacencyList(int SIZE);
     ~AdjacencyList();
+    void AddEdge(int x,int y,int weight);
+    void RemoveEdge(int x,int y);
+    void CountDensity();
+    float GiveDensity();
+    private:
+    std::vector<std::vector<ListNode>> List; 
 };
